680f15aaf43d72d08fd43837ee12eb60506fa1a8
JkyProgrammer <32058792+JkyProgrammer@users.noreply.github.com>
2020-04-25 16:55:48 +0100

    Implemented a bunch of functions

---
+++ elementv3/elementv3.cc
@@ -0,0 +1,174 @@
+// TODO: Write up
+#include "elementv3.h"
+
+void structure::call (int i) {
+    if (motorNum != -1) {
+        if (i > THRESHOLD) {
+            buffer->motorCall (motorNum);
+        } else {
+            charge = i;
+        }
+        return;
+    }
+    if (i > THRESHOLD) {
+        int numLinks = sizeof (links)/sizeof(structure*);
+        int chargePerLink = (i + charge)/numLinks;
+        for (structure *s : links) {
+            buffer->queueLock.lock();
+            queue<int> q;
+            charge_i ci;
+            ci.c = chargePerLink;
+            ci.t = s;
+            buffer->queue.push (ci);
+            buffer->queueLock.unlock();
+        }
+    } else {
+        charge = i;
+    }
+}
+
+void structure::setLinks (structure *ls[MAX_LINKS]) {
+    for (int i = 0; i < MAX_LINKS; i++) links[i] = ls[i];
+}
+
+structure::structure (structurebuffer* buf) {
+    buffer = buf;
+}
+structure::structure (structurebuffer* buf, int mot) {
+    motorNum = mot;
+    buffer = buf;
+}
+
+structurebuffer::structurebuffer () {
+    for (int i = 0; i < NUM_WORKER_THREADS; i++) {
+        thread *t = new thread (threadStart);
+        threads.push_back (t);
+    }
+    generateNodes ();
+}
+
+structurebuffer::structurebuffer (string path) {
+    for (int i = 0; i < NUM_WORKER_THREADS; i++) {
+        thread *t = new thread (threadStart);
+        threads.push_back (t);
+    }
+    readIn (path);
+}
+
+void structurebuffer::threadStart () {
+    while (!waitingForClose) {
+        queueLock.lock();
+        charge_i i = queue.front();
+        queue.pop();
+        queueLock.unlock();
+        i.t->call(i.c);
+        this_thread::sleep_for (chrono::milliseconds(WORKER_DELAY));
+    }
+}
+
+int structurebuffer::indexOfNodeInNet (structure* s) {
+    int i = 0;
+    for (structure *ss : buffer) {
+        if (ss == s) return i;
+        i++;
+    }
+    return -1;
+}
+
+void structurebuffer::writeOut (string path) {
+    ofstream file;
+    file.open (path);
+    for (structure *struc : buffer) {
+        for (structure *link : struc->links) {
+            file << to_string (indexOfNodeInNet (link)) << ":";
+        }
+        file << "\n";
+    }
+    file << "<i";
+    for (structure *istr : sensors) {
+        file << to_string(indexOfNodeInNet(istr)) << ":";
+    }
+    file << "\n";
+
+    file << "<o";
+    for (structure *ostr : motors) {
+        file << to_string(indexOfNodeInNet(ostr)) << ":";
+    }
+    file << "\n";
+
+    file.close();
+}
+
+vector<int> getIndices (string s) {
+    vector<int> arr;
+    arr.reserve (MAX_LINKS);
+    int j = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ':') j++;
+        else arr[j] += s[i];
+    }
+    return arr;
+}
+
+void structurebuffer::readIn (string path) {
+    ifstream file;
+    file.open (path);
+    if (!file.is_open) return;
+    string line;
+    vector<string> nodelines;
+    string ins;
+    string outs;
+    while (getline (file, line)) {
+        if (line[0] != '<') {
+            nodelines.push_back(line);
+            buffer.push_back (new structure (this));
+        } else {
+            if (line[1] == 'i') {
+                ins = line.substr (2);
+            } else if (line[1] == 'o') {
+                outs = line.substr (2);
+            }
+        }
+    }
+    file.close();
+    for (int i = 0; i < nodelines.size(); i++) {
+        structure *links[MAX_LINKS];
+        int j = 0;
+        for (int n : getIndices (nodelines[i])) {
+            links[j] = buffer[n];
+            j++;
+        }
+        buffer[i]->setLinks (links);
+    }
+    for (int in : getIndices(ins)) {
+        sensors.push_back (buffer[in]);
+    }
+    int x = 0;
+    for (int on : getIndices(outs)) {
+        motors.push_back (buffer[on]);
+        structure *swapout = buffer[on];
+        structure *n = new structure (this, x);
+        n->setLinks (swapout->links);
+        buffer[on] = n;
+        x++;
+    }
+}
+
+void structurebuffer::generateNodes () {
+    // TODO: Generate nodes
+}
+
+void structurebuffer::modify (int) {
+    // TODO: Modify the net
+}
+
+void structurebuffer::insertRandomNode () {
+    // TODO: Insert a random node
+}
+
+void structurebuffer::triggerInput (int, int) {
+    // TODO: Pass input
+}
+void structurebuffer::motorCall (int) {
+    // TODO: Receive a motor call
+}
--- elementv3/elementv3.h
+++ elementv3/elementv3.h
@@ -3,6 +3,8 @@
 #include <queue>
 #include <thread>
 #include <mutex>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -11,16 +13,22 @@ using namespace std;
 #define ELEMENT
 
 #define THRESHOLD 10
-#define MAX_LINKS
+#define MAX_LINKS 8
+#define NUM_WORKER_THREADS 8
+#define WORKER_DELAY 10
 
 class structure {
 public:
     void call (int);				// Send an impulse to this structure
 
-    structure ();					// Default constructor
-private:
+    structure (structurebuffer *);	// Default constructor
+	structure (structurebuffer*, int);
+	void setLinks (structure *[MAX_LINKS]);
+	structure* links[MAX_LINKS];	// List of outgoing connections to other structure
+protected:
     unsigned char charge;			// The amount of charge hanging around in the structure
-    structure* links[MAX_LINKS];	// List of outgoing connections to other structure
+	structurebuffer *buffer;
+	int motorNum = -1;
 };
 
 struct charge_i {
@@ -28,7 +36,6 @@ struct charge_i {
 	structure *t;
 };
 
-// TODO: Write up
 class structurebuffer {
 public:
 	mutex queueLock;
@@ -38,34 +45,30 @@ public:
     vector<structure*> sensors; // List of sensor nodes associated with this structurebuffer, always is a subset of buffer
     vector<structure*> motors; // List of motor nodes associated with this structurebuffer, always is a subset of buffer
 
-	vector<thread *> threads;
-	bool waitingForClose = false;
+	vector<thread *> threads; // Keeps the threads allocated
+	bool waitingForClose = false; // Allows us to turn off the net and safely close all the threads
 
     // Initialiser taking argument for whether or not to automatically modify the net on update
-    structurebuffer (bool m);
+    structurebuffer ();
 
     // Initialiser taking arguments for whether or not to automatically modify the net on update, and for a path to an existing net file to read in
-    structurebuffer (bool m, string path);
+    structurebuffer (string);
 
-
-
-    // Add a new, premade sensor node to the structurebuffer
-    void addSensor (structure *s);
-    // Add a new motor node to the structurebuffer, automatically created, assigning the argument as a pointer to the motor handler function
-    void addMotor (void (*ah) (int));
+	int indexOfNodeInNet (structure*);
 
     // Write out the entire net to a file which can later be read back in (resuming as before)
-    void writeOut (string path);
-	void readIn (string path);
+    void writeOut (string);
+	void readIn (string);
 
-    // Randomly change the net slightly
-    void modify (int iterations);
-
-    // Insert a new node randomly into the net
-    void insertRandomNode ();
+    void generateNodes (); // Generate a new net
+    void modify (int); // Randomly change the net slightly
+    void insertRandomNode (); // Insert a new node randomly into the net
 
     // Trigger an input on a sensor node with a given strength value
-    void triggerInput (int sensorNum, int charge);
+    void triggerInput (int, int);
+	void motorCall (int);
+
+	void threadStart ();
 };
 
 #endif
